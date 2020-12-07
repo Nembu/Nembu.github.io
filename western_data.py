ff505b5b5c0c1c984062e2e4deb4fb203c3db100
Nembu <69772494+Nembu@users.noreply.github.com>
2020-12-07 10:34:34 -0800

    Add files via upload

---
+++ western_data.py
@@ -0,0 +1,25 @@
+from browser import document, window
+Plotly = window.Plotly
+
+def unpack(rows,key):
+	res = []
+	for row in rows:
+		res.append(row[key])
+	return res
+
+def csv_load_callback(err, rows):
+	dates = unpack(rows, 'date')
+	
+	tc = unpack(rows, 'total_cases')
+	
+	tc_data = [
+			{
+	        "x": dates,
+	        "y": tc,
+	        "mode": 'markers',
+	        "type": 'scatter'
+	    }];
+	
+	Plotly.newPlot('tc', tc_data);
+
+Plotly.d3.csv("https://raw.githubusercontent.com/Jack2104/covid-data/main/western_hemisphere_data.csv", csv_load_callback)
